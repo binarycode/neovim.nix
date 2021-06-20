3551300ffe3a86475c49a67e9c508bea56080710
Igor Sidorov <igor.sidorov@binarycode.ru>
2021-06-20 14:39:10 +0300

    replace broken upstream of vim-grepper with a fork

--- plugins/vim-grepper.nix
+++ plugins/vim-grepper.nix
@@ -18,5 +18,21 @@
     ''
   ];
 
-  neovimPlugins = [ pkgs.vimPlugins.vim-grepper ];
+  neovimPlugins = [
+    # upstream plugin has issue with ripgrep integration, using fork meanwhile
+    # https://github.com/mhinz/vim-grepper/issues/244
+    # TODO: revisit this later and revert to upstream
+    # pkgs.vimPlugins.vim-grepper
+
+    (pkgs.vimUtils.buildVimPluginFrom2Nix {
+      pname = "vim-grepper";
+      version = "2020-06-20";
+      src = pkgs.fetchFromGitHub {
+        owner = "binarycode";
+        repo = "vim-grepper";
+        rev = "2171b2eff948e529266bba3cad463415fdd9cfb5";
+        sha256 = "11q9zyz6lr3kycgw2kg6ih2ldaiap5im7rrc85pi6ffdmhm849s5";
+      };
+    })
+  ];
 }

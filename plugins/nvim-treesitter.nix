54c6146544250355da23ee30c79392e6423570d7
Igor Sidorov <igor.sidorov@binarycode.ru>
2021-11-18 22:34:44 +0300

    attempt to fix an issue

--- plugins/nvim-treesitter.nix
+++ plugins/nvim-treesitter.nix
@@ -2,5 +2,5 @@
   # https://github.com/nvim-treesitter/nvim-treesitter
   # treesitter support
 
-  my.neovimPlugins = [ pkgs.vimPlugins.nvim-treesitter.withPlugins (_: pkgs.tree-sitter.allGrammars) ];
+  my.neovimPlugins = [ (pkgs.vimPlugins.nvim-treesitter.withPlugins (_: pkgs.tree-sitter.allGrammars)) ];
 }

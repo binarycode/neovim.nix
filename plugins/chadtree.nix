fa6d072b8077d248fb9f6db295c8aaf1b06bc985
Igor Sidorov <igor.sidorov@binarycode.ru>
2021-12-29 15:58:35 +0300

    attempt to install chadtree dependencies automatically

--- plugins/chadtree.nix
+++ plugins/chadtree.nix
@@ -2,5 +2,12 @@
   # https://github.com/ms-jpq/chadtree
   # file manager
 
-  my.neovimPlugins = [ pkgs.vimPlugins.chadtree ];
+  my.neovimPlugins = [
+    (pkgs.vimPlugins.chadtree.overrideAttrs (_: {
+      buildInputs = [ pkgs.python3 ];
+      buildPhase = ''
+        python3 -m chadtree deps --nvim
+      '';
+    }))
+  ];
 }

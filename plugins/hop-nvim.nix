6ba36e1beed278db324b40f661560a97f9e0467b
Igor Sidorov <igor.sidorov@binarycode.ru>
2021-12-29 11:22:43 +0300

    hop.nvim should work in visual mode

--- plugins/hop-nvim.nix
+++ plugins/hop-nvim.nix
@@ -8,14 +8,17 @@
         " move to char
         map  <Leader>f :HopChar1<CR>
         nmap <Leader>f :HopChar1<CR>
+        vmap <Leader>f <CMD>HopChar1<CR>
 
         " move to word
         map  <Leader>w :HopWord<CR>
         nmap <Leader>w :HopWord<CR>
+        vmap <Leader>w <CMD>HopWord<CR>
 
         " move to line
         map  <Leader>l :HopLine<CR>
         nmap <Leader>l :HopLine<CR>
+        vmap <Leader>l <CMD>HopLine<CR>
 
         " required for default hint colors to be set up
         lua require'hop'.setup { }
@@ -27,12 +30,18 @@
       ''
         " move to char
         map  <Leader>f :HopChar1<CR>
+        nmap <Leader>f :HopChar1<CR>
+        vmap <Leader>f <CMD>HopChar1<CR>
 
         " move to word
         map  <Leader>w :HopWord<CR>
+        nmap <Leader>w :HopWord<CR>
+        vmap <Leader>w <CMD>HopWord<CR>
 
         " move to line
         map  <Leader>l :HopLine<CR>
+        nmap <Leader>l :HopLine<CR>
+        vmap <Leader>l <CMD>HopLine<CR>
 
         " required for default hint colors to be set up
         lua require'hop'.setup { }

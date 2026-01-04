7b48bcc583237fdfe052c8f814ade2f987c0960c
Igor Sidorov <igor.sidorov@binarycode.ru>
2026-01-04 11:41:38 +0200

    oops missed a spot

--- plugins/gitsigns/default.nix
+++ plugins/gitsigns/default.nix
@@ -1,12 +1,4 @@
 {pkgs, ...}: {
-  plugins.gitsigns = {
-    enable = true;
-    lazyLoad.settings.lazy = true;
-    settings = {
-      numhl = true;
-      signcolumn = false;
-    };
-  };
   extraPlugins = [
     {
       plugin = pkgs.vimPlugins.gitsigns-nvim;

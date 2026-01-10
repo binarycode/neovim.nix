2fc9082fcb80bd9c59f7f73ce5b985d396e9fce9
Igor Sidorov <igor.sidorov@binarycode.ru>
2026-01-10 11:16:27 +0200

    fix diffview closing

--- plugins/neogit/config.lua
+++ plugins/neogit/config.lua
@@ -2,9 +2,28 @@ if not vim.g.vscode then
   vim.cmd.packadd("diffview.nvim")
   vim.cmd.packadd("neogit")
 
-  local neogit = require("neogit")
+  require("diffview").setup({
+    keymaps = {
+      view = {
+        q = "<Cmd>DiffviewClose<CR>",
+      },
+      file_panel = {
+        q = "<Cmd>DiffviewClose<CR>",
+      },
+      file_history_panel = {
+        q = "<Cmd>DiffviewClose<CR>",
+      },
+    },
+    hooks = {
+      view_opened = function()
+        vim.cmd("DiffviewToggleFiles")
+      end,
+    },
+  })
 
-  vim.keymap.set("n", "<Leader>z", neogit.open, {
+  local plugin = require("neogit")
+
+  vim.keymap.set("n", "<Leader>z", plugin.open, {
     silent = true,
     desc = "Neogit",
   })

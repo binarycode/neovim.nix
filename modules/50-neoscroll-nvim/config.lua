f4bd77e8e76cf073326126419b4a974947987b17
Igor Sidorov <igor.sidorov@binarycode.ru>
2026-04-06 18:54:54 +0300

    use neoscroll for ctrl-j and ctrl-k

--- modules/50-neoscroll-nvim/config.lua
+++ modules/50-neoscroll-nvim/config.lua
@@ -1,5 +1,14 @@
 if not vim.g.vscode then
   vim.cmd.packadd("neoscroll.nvim")
 
-  require("neoscroll").setup()
+  local plugin = require("neoscroll")
+
+  plugin.setup()
+
+  vim.keymap.set("n", "<C-j>", function()
+    plugin.ctrl_f({ duration = 200 })
+  end, { silent = true })
+  vim.keymap.set("n", "<C-k>", function()
+    plugin.ctrl_b({ duration = 200 })
+  end, { silent = true })
 end

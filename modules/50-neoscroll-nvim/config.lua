d17f9b8a5fc14faafc2a9cb7bcbe8e69e7beb395
Igor Sidorov <igor.sidorov@binarycode.ru>
2026-04-09 12:26:16 +0300

    using neoscroll for large scrolls was a bad idea

--- modules/50-neoscroll-nvim/config.lua
+++ modules/50-neoscroll-nvim/config.lua
@@ -1,14 +1,5 @@
 if not vim.g.vscode then
   vim.cmd.packadd("neoscroll.nvim")
 
-  local plugin = require("neoscroll")
-
-  plugin.setup()
-
-  vim.keymap.set("n", "<C-j>", function()
-    plugin.ctrl_f({ duration = 200 })
-  end, { silent = true })
-  vim.keymap.set("n", "<C-k>", function()
-    plugin.ctrl_b({ duration = 200 })
-  end, { silent = true })
+  require("neoscroll").setup()
 end
